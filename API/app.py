5667ba074edad3ff0ceaf372d07969f46bbec870
eusoucarlitos <carlitosmartinezbrasil@gmail.com>
2026-05-27 08:58:57 -0300

    Update app.py

--- API/app.py
+++ API/app.py
@@ -2,3 +2,31 @@ from flask import Flask, jsonify, request
 from sensor import sensorAgua
 from LED import LED
 
+app = Flask(__name__)
+
+# GET = Pegar dodos do site
+# POST = Enviar dados
+# rotas = o principal/algo/_algo_
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/analise", methods=["GET", "POST"])
+def analise():
+    if request.method == "POST":
+        Altura_Atual = request.form ['Atual']
+        Altura_Limite = request.form ['Limite']
+
+        sensor = (
+            "Nível",
+            Altura_Atual,
+            Altura_Limite
+        )
+        Dado = sensor.Analise()
+        
+        return f"Dado enviado, resposta: {Dado}"
+
+#Iniciar a host + servidor local
+if __name__ == "__main__":
+    app.run()
