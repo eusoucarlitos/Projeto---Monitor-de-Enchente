6e3519ed0e6bfd7b303f282519c5ee83e9be24a4
eusoucarlitos <carlitosmartinezbrasil@gmail.com>
2026-05-27 08:48:45 -0300

    Update LED.py
    Criação da classe LED

--- API/LED.py
+++ API/LED.py
@@ -1,1 +1,31 @@
+class Atuador:
+  
+  def __init__(self, nome, pino):
+    self._nome = nome
+    self._pino = pino
+    self._estado = False
 
+  def get_estado(self):
+    return self._estado
+
+  def ligar(self):
+    self._estado = True
+    print(f{self._nome} ligado)
+    return self._estado
+
+  def desligar(self):
+    self._estado = False
+    print(f{self._nome} desligado)
+    return self._estado
+    
+  def alternar(self):
+      if self._estado:
+        return self.desligar()
+      else:
+        return self.ligar()
+
+ class LED(Atuador):
+
+  def __init__(self, pino, cor=vermelho):
+    super().__init__(fLED {cor}, pino)
+    self._cor = cor
