c9fa85cfef438f026ad14d088d02009929beac84
RonaldVix <pessoalenzzo@gmail.com>
2026-07-22 07:33:26 -0300

    Update arduinocode.cpp

--- Arduino/arduinocode.cpp
+++ Arduino/arduinocode.cpp
@@ -4,7 +4,6 @@
 ==========================================================
 
 Altura total do reservatório: 300 cm
-
 Classificação:
 
 0 - 99 cm      -> Baixo
@@ -20,41 +19,27 @@ O Arduino envia os dados em formato JSON.
 //==========================================================
 
 class SensorUltrassonico {
-
   private:
-
     int triggerPin;
     int echoPin;
-
   public:
-
     SensorUltrassonico(int trig, int echo) {
-
       triggerPin = trig;
       echoPin = echo;
-
       pinMode(triggerPin, OUTPUT);
       pinMode(echoPin, INPUT);
-
     }
 
     float obterDistanciaCM() {
-
       digitalWrite(triggerPin, LOW);
       delayMicroseconds(2);
-
       digitalWrite(triggerPin, HIGH);
       delayMicroseconds(10);
-
       digitalWrite(triggerPin, LOW);
-
       long tempo = pulseIn(echoPin, HIGH);
-
       float distancia = tempo * 0.0343 / 2;
-
       return distancia;
     }
-
 };
 
 //==========================================================
@@ -62,37 +47,24 @@ class SensorUltrassonico {
 //==========================================================
 
 class IndicadorLED {
-
   private:
-
     int pino;
-
   public:
-
     IndicadorLED(int led) {
-
       pino = led;
-
     }
 
     void iniciar() {
-
       pinMode(pino, OUTPUT);
-
     }
 
     void ligar() {
-
       digitalWrite(pino, HIGH);
-
     }
 
     void desligar() {
-
       digitalWrite(pino, LOW);
-
     }
-
 };
 
 //==========================================================
@@ -103,7 +75,6 @@ class IndicadorLED {
 // Echo = 3
 
 SensorUltrassonico sensor(2, 3);
-
 IndicadorLED led(LED_BUILTIN);
 
 //==========================================================
@@ -117,11 +88,8 @@ const float ALTURA_TOTAL = 300.0;
 //==========================================================
 
 void setup() {
-
   Serial.begin(9600);
-
   led.iniciar();
-
 }
 
 //==========================================================
@@ -129,26 +97,20 @@ void setup() {
 //==========================================================
 
 void loop() {
-
   //--------------------------------------------
   // Distância entre sensor e água
   //--------------------------------------------
-
   float distancia = sensor.obterDistanciaCM();
-
   //--------------------------------------------
   // Altura da água
   //--------------------------------------------
-
   float alturaAgua = ALTURA_TOTAL - distancia;
-
   //--------------------------------------------
   // Evita valores inválidos
   //--------------------------------------------
 
   if (alturaAgua < 0)
     alturaAgua = 0;
-
   if (alturaAgua > ALTURA_TOTAL)
     alturaAgua = ALTURA_TOTAL;
 
@@ -157,26 +119,19 @@ void loop() {
   //--------------------------------------------
 
   String perigo;
-
   if (alturaAgua < 100) {
-
     perigo = "Baixo";
     led.desligar();
-
   }
 
   else if (alturaAgua < 200) {
-
     perigo = "Medio";
     led.desligar();
-
   }
 
   else {
-
     perigo = "Alto";
     led.ligar();
-
   }
 
   //--------------------------------------------
@@ -184,27 +139,20 @@ void loop() {
   //--------------------------------------------
 
   Serial.print("{");
-
   Serial.print("\"altura\":");
   Serial.print(alturaAgua, 1);
-
   Serial.print(",");
-
   Serial.print("\"perigo\":\"");
   Serial.print(perigo);
   Serial.print("\"");
-
   Serial.print(",");
-
   Serial.print("\"led\":");
-
+        
   if (alturaAgua >= 200)
     Serial.print("true");
   else
     Serial.print("false");
-
   Serial.println("}");
-
   delay(1000);
-
 }
+
